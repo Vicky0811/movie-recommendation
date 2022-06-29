16ed8beb9cef47b3622005030475a9d9725a825c
Vicky0811 <108380639+Vicky0811@users.noreply.github.com>
2022-06-29 12:58:19 +0530

    Update movie recomm.py

--- movie recomm.py
+++ movie recomm.py
@@ -177,32 +177,8 @@ for  movie in sorted_similar_movies:
         print(i,'.',title_from_index)
         i+=1
 
-
-# In[ ]:
-
-
-#Movie recommendation system
-
-movie_name = input('Enter your favorite movie name: ')
-find_close_match = difflib.get_close_matches(movie_name,list_of_all_titles)
-close_match = find_close_match[0]
-index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
-similarity_score = list(enumerate(similarity[index_of_the_movie]))
-sorted_similar_movies = sorted(similarity_score,key = lambda x:x[1],reverse = True)
-print('movies suggested for you :  ')
-i=1
-for  movie in sorted_similar_movies:
-    index = movie[0]
-    title_from_index = movies_data[movies_data.index==index]['title'].values[0]
-    if(i<=90):
-        print(i,'.',title_from_index)
-        i+=1
-
-
-# In[ ]:
-
-
-# movie recommendation system
+#collectively
+# with a new gerns movie recommendation system
 
 selected_features = ['genres','keywords','tagline','cast','director']
 for feature in selected_features:
